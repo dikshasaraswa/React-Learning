9fcbecc34ec533303b956565a2124d532b0a9334
dikshasarasw <renusaraswat620@gmail.com>
2024-07-09 18:31:27 +0530

    first

---
+++ app.js
@@ -0,0 +1,18 @@
+    //creating h1 tag using react 
+    const heading1 = React.createElement("h1",{
+        id:"title",
+    },"HEADING 1");
+
+    const heading2 = React.createElement("h2",{
+        id:"title",
+    },"HEADING 2");
+
+    const container = React.createElement("div",{
+        id:"conatiner",
+    },[heading1,heading2]);
+
+    const root = ReactDOM.createRoot(document.getElementById("root"));
+
+   //console.log(heading); a react element is a object 
+   
+    root.render(container);//passing a react element inside root
