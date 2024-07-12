1c1c600dff2b4d29e268113d2e1314ef73b92b58
dikshasarasw <renusaraswat620@gmail.com>
2024-07-13 00:07:13 +0530

    f

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
-    
+    import React from "react";
+    import ReactDOM from "react-dom/client";
     //creating h1 tag using react 
     const heading1 = React.createElement("h1",{
         id:"title",
-    },"HEADING 1");
+    },"HEADING 1 from parcel");
 
     const heading2 = React.createElement("h2",{
         id:"title",
-    },"HEADING 2");
+    },"HEADING 2 diksha");
 
     const container = React.createElement("div",{
         id:"container",
